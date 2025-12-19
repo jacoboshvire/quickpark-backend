bca3494895a93a375209700407f7db1b9261dfd9
Juliet Ekuahorovwe <enoh@juliets-air.home>
2025-12-19 12:56:58 +0000

    just force commit

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,22 +1,25 @@
-const admin = require("firebase-admin/app");
-const { getMessaging } = require("firebase-admin/messaging");
-const { cert } = require("firebase-admin/app");
+const admin = require("firebase-admin");
+let serviceAccount;
 
+// Load credentials
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  // Production
+  // Production (Base64 env var)
   serviceAccount = JSON.parse(
     Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf8")
   );
 } else {
-  // Local development
-  serviceAccount = require(FIREBASE_URL);
+  // Local development (JSON file, gitignored)
+  serviceAccount = require("../quickpark-278e6-firebase-adminsdk-fbsvc-df2cc889af.json");
 }
 
+// Initialize Firebase ONCE
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
-admin.initializeApp({
-  credential: cert(serviceAccount),
-});
+module.exports = admin;
 
-module.exports = { admin, getMessaging };
 
 
