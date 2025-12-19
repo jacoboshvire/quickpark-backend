6213424876c73f1fc01801c4db6ec5be2921831d
Juliet Ekuahorovwe <enoh@juliets-air.home>
2025-12-19 12:38:16 +0000

    committed@1238 191225

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,11 +2,21 @@ const admin = require("firebase-admin/app");
 const { getMessaging } = require("firebase-admin/messaging");
 const { cert } = require("firebase-admin/app");
 
-const serviceAccount = require(process.env.FIREBASE_URL);
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // Production
+  serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf8")
+  );
+} else {
+  // Local development
+  serviceAccount = require(FIREBASE_URL);
+}
+
 
 admin.initializeApp({
   credential: cert(serviceAccount),
 });
 
 module.exports = { admin, getMessaging };
+
 
